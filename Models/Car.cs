dd53aa8fc96991ba8320857ad613392b3cda0f55
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-05-03 11:46:17 -0700

    finish the lesson for cardealership
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

---
+++ Models/Car.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Dealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public static string MakeSound(string sound)
+    {
+      return "Our cars sound like " + sound;
+    }
+
+    public string GetMakeModel()
+    {
+      return MakeModel;
+    }
+
+    public int GetPrice()
+    {
+      return Price;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      Price = newPrice;
+    }
+
+    public int GetMiles()
+    {
+      return Miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
